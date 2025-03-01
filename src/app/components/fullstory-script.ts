fa53c296ed08632df64bee34e5ed078331fb550c
Matt Carl <mccarl@asu.edu>
2025-03-01 15:20:50 -0500

    Update FS script

--- src/app/components/fullstory-script.ts
+++ src/app/components/fullstory-script.ts
@@ -5,7 +5,7 @@ import { useEffect } from "react"
 export function FullStoryScript() {
     useEffect(() => {
         window._fs_host = window._fs_host || "fullstory.com"
-        const _fs_script = "edge.fullstory.com/s/fs.js"
+        const _fs_script = "https://edge.fullstory.com/s/fs.js"
         window._fs_script = _fs_script
         window._fs_org = process.env.FS_ORG_ID || "o-2121BN-na1"
         window._fs_namespace = "FS"
