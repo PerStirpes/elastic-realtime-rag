2688f12a780f53dd92683efc4e1fd52d1e9d3cb4
Matt Carl <mccarl@asu.edu>
2025-03-06 19:03:50 -0500

    add breakdownMetrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
               serviceName: 'realtime-frontend',
               serverUrl: 'https://abbe56903b824f85b28892772f898006.apm.us-east-1.aws.cloud.es.io:443',
               environment: '${process.env.NODE_ENV || "development"}',
+              breakdownMetrics: true,
             });
           `}
                 </Script>
